4bfda89f1b14df0578a0bfb53e86a5f409ae9af9
itssss-sweta <swetamhrjn252@gmail.com>
2024-08-30 07:59:55 +0545

    feat: configure application.properties and add tabel connection in entity class.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	String home(){
 		return "Hello World";
 	}
