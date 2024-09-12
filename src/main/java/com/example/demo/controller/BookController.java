eb0d886a918367242a84032778f9c0b9e9cf34f0
itssss-sweta <swetamhrjn252@gmail.com>
2024-09-12 07:44:35 +0545

    feat: alter methods to get db data instead of arraylist in controller

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,32 +13,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
-    private List<Book> books = new ArrayList<>();
+
+    final BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+   }
+
 
     @GetMapping
-    public List<Book> getAllBooks(){
-        return books;
+    public ResponseEntity<List<Book>> getAllBooks(){
+        List<Book> fetchBooks = bookService.getBook();
+        return ResponseEntity.ok(fetchBooks);
     }
 
     @PostMapping
-    public String createBook(@RequestBody Book book){
-        books.add(book);
-        return "Book Inserted: " + book.getBookTitle();
+    public ResponseEntity<Book> createBook(@RequestBody Book book){
+        Book postBook = bookService.postBook(book);
+        return ResponseEntity.ok(postBook);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteBook(@PathVariable long id){
-        for(Book book : books){
-            if(book.getId() == id){
-                books.remove(book);
-                return book.getBookTitle() + "with Book Id" + id +"has been deleted.";
-            }
+    public ResponseEntity<String> deleteBook(@PathVariable long id){
+      boolean deleteStatus = bookService.deleteBook(id);
+      if (deleteStatus) {
+            return ResponseEntity.ok("Product with ID " + id + " has been deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete product with ID " + id);
         }
-        return "Book with ID " + id + " not found.";
     }
-
 }
