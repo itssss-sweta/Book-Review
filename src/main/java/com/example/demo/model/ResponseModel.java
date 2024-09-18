2a2429b80b9c504e28071c1c5ebbedc4078d2a76
itssss-sweta <swetamhrjn252@gmail.com>
2024-09-18 07:14:19 +0545

    feat: create response model

--- src/main/java/com/example/demo/model/ResponseModel.java
+++ src/main/java/com/example/demo/model/ResponseModel.java
@@ -1,5 +1,35 @@
 package com.example.demo.model;
 
-public class ResponseModel {
+public class ResponseModel<T> {
+    private String message;
+    private int status;
+    private T data;
+
+    public ResponseModel(int status,String message,T data){
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public int getStatus(){
+        return status;
+    }
+    public String getMessage(){
+        return message;
+    }
+    public T getData(){
+        return data;
+    }
+
+    public void setStatus(int status){
+        this.status = status;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    public void setData(T data){
+        this.data = data;
+    }
+
     
 }
