daef8757d56e7038bba411ddf3d722a41c92113a
itssss-sweta <swetamhrjn252@gmail.com>
2024-09-20 14:55:44 +0545

    feat: add methods for conflict and unauthorized response in response utils

--- src/main/java/com/example/demo/utils/ResponseUtil.java
+++ src/main/java/com/example/demo/utils/ResponseUtil.java
@@ -5,12 +5,11 @@ import org.springframework.http.ResponseEntity;
 
 import com.example.demo.model.ResponseModel;
 
-
 public class ResponseUtil {
 
     // 200 OK with data
     public static <T> ResponseEntity<ResponseModel<T>> successResponse(T data, String message) {
-        ResponseModel<T> response = new ResponseModel<>( HttpStatus.OK.value(), message, data);
+        ResponseModel<T> response = new ResponseModel<>(HttpStatus.OK.value(), message, data);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
@@ -32,10 +31,22 @@ public class ResponseUtil {
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
+    // 401 Unauthorized
+    public static <T> ResponseEntity<ResponseModel<T>> unauthorizedResponse(String message) {
+        ResponseModel<T> response = new ResponseModel<>(HttpStatus.UNAUTHORIZED.value(), message, null);
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
     // 404 Not Found
     public static <T> ResponseEntity<ResponseModel<T>> notFoundResponse(String message) {
         ResponseModel<T> response = new ResponseModel<>(HttpStatus.NOT_FOUND.value(), message, null);
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    // 409 Conflict
+    public static <T> ResponseEntity<ResponseModel<T>> conflictResponse(String message) {
+        ResponseModel<T> response = new ResponseModel<>(HttpStatus.CONFLICT.value(), message, null);
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
     // 500 Internal Server Error
@@ -43,4 +54,4 @@ public class ResponseUtil {
         ResponseModel<T> response = new ResponseModel<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), message, null);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-} 
+}
