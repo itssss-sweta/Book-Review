7d27b39576c0e5670c30efc53dfc620ddadeb601
itssss-sweta <swetamhrjn252@gmail.com>
2024-09-20 16:42:46 +0545

    feat: create dependency for secutiryconfig with user service

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,45 +1,58 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.demo.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails userDetails = User.builder().username("user").password("{noop}password").roles("USER").build();
-    return new InMemoryUserDetailsManager(userDetails);
+  private UserService userService;
+
+  @Autowired
+  public SecurityConfig(@Lazy UserService userService) {
+    this.userService = userService;
   }
 
-  // @Bean
-  // public PasswordEncoder passwordEncoder() {
-  // return new BCryptPasswordEncoder();
-  // }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http)
+  public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager)
       throws Exception {
     return http.cors(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-            // .requestMatchers(HttpMethod.POST, "/api/auth/signup/**").permitAll()
-            // .requestMatchers(HttpMethod.POST, "/api/auth/login/**").permitAll()
-            // .requestMatchers(HttpMethod.GET, "/authentication-docs/**").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/signup/**").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/login/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/authentication-docs/**").permitAll()
             .anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults()).build();
+        .authenticationManager(authenticationManager).build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    AuthenticationManagerBuilder authenticationManagerBuilder = http
+        .getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    return authenticationManagerBuilder.build();
   }
 
 }
