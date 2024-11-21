67289d0918476e17030a5bd600e9c7949c8bcb0b
itssss-sweta <swetamhrjn252@gmail.com>
2024-11-21 11:56:27 +0545

    feat: update Book model with new fields

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -10,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -41,7 +43,30 @@ public class Book {
     @Column(nullable = false)
     private double price;
 
+    @Column(nullable = false)
     private String imageUrl;
+
+    @Column(nullable = false)
+    private double rating;
+
+    @Column(nullable = false)
+    private String publisher;
+
+    @Column(nullable = false)
+    private int publicationYear;
+
+    @Lob // This annotation ensures the field is treated as a large object (LOB) in MySQL
+    @Column(columnDefinition = "LONGTEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private String edition;
+
+    @Column(nullable = false)
+    private List<String> genres;
+
+    @Column(nullable = false)
+    private int pageCount;
 
     @ManyToMany(mappedBy = "favouriteBooks")
     @JsonIgnore
